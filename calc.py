89c80e85b4666d02e8f76e4d6a0dd74bd8a3a7fa
Bibind <bvasu@uchicago.edu>
2019-08-26 20:56:49 -0500

    Multiply function

--- calc.py
+++ calc.py
@@ -5,3 +5,17 @@ Created on Mon Aug 26 19:29:20 2019
 @author: bibin
 """
 
+def add(x,y):
+    pass
+
+def subtract(x,y):
+    pass x
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    pass
+
+def square(x):
+    pass
