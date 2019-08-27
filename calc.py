fccb244e0a8ef36907a088740432324043090b68
Bibind <bvasu@uchicago.edu>
2019-08-26 22:12:47 -0500

    Subtract commit

--- calc.py
+++ calc.py
@@ -9,13 +9,13 @@ def add(x,y):
     pass
 
 def subtract(x,y):
-    pass x
+    return x-y
 
 def multiply(x,y):
     return x*y
 
 def divide(x,y):
-    pass
+    return x/y
 
 def square(x):
     pass
